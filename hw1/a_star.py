7b22a2e4737006d593be61fd37c0cecddb3214a8
Gabriel <gabriel.appleby@gmail.com>
2018-10-02 13:37:38 -0400

    Ref T22: Cleaning up project for submission.

--- hw1/a_star.py
+++ hw1/a_star.py
@@ -1,28 +1,37 @@
 from pancake_stack import PancakeStack
 from cake_heap import CakeHeap
-from typing import Dict
+from typing import Dict, Tuple, List, Optional
 
 
 def a_star(initial_state: PancakeStack, goal_state: PancakeStack) -> \
-        Dict[PancakeStack, PancakeStack]:
-    frontier = CakeHeap()
-    visited = []
-    path = {initial_state: None}
+        Optional[Tuple[Dict[PancakeStack, PancakeStack], PancakeStack]]:
+    """
+    Given an initial state and a goal state finds a series of flips to get
+    from the initial state to the goal using the a* algorithm.
+    :param initial_state: The initial state to get to the goal from.
+    :param goal_state: The goal state to get to from the initial state.
+    :return: A tuple containing: a dictionary where each key is a state, and
+    each value is that state's parent, and the goal state. None is returned if
+    no path is found.
+    """
+    frontier: CakeHeap = CakeHeap()
+    visited: List = []
+    path: Dict[PancakeStack, PancakeStack] = {initial_state: None}
     frontier.push(initial_state, 0)
 
     while not frontier.empty():
-        current_state = frontier.pop()
+        current_state: PancakeStack = frontier.pop()
         visited.append(current_state)
         if goal_state == current_state:
-            return path
-        children = current_state.children()
-        for child in children:
+            return path, current_state
+        children: List[PancakeStack] = current_state.children()
+        for child in children:  # type: PancakeStack
             if child not in visited and child not in frontier:
                 frontier.push(child, child.cost_to_goal())
                 path[child] = current_state
             elif child in frontier and frontier[child].cost_to_goal() > \
                     child.cost_to_goal():
                 frontier.remove(child)
-                frontier.push(child, child.cost_to_goal)
+                frontier.push(child, child.cost_to_goal())
                 path[child] = current_state
-    return {}
+    return None
--- hw1/uniform_cost.py
+++ hw1/uniform_cost.py
@@ -1,22 +1,31 @@
 from pancake_stack import PancakeStack
 from cake_heap import CakeHeap
-from typing import Dict
+from typing import Dict, Tuple, List, Optional
 
 
 def uniform_cost(initial_state: PancakeStack, goal_state: PancakeStack) -> \
-        Dict[PancakeStack, PancakeStack]:
-    frontier = CakeHeap()
-    visited = []
-    path = {initial_state: None}
+        Optional[Tuple[Dict[PancakeStack, PancakeStack], PancakeStack]]:
+    """
+    Given an initial state and a goal state finds a series of flips to get
+    from the initial state to the goal using the uniform cost algorithm.
+    :param initial_state: The initial state to get to the goal from.
+    :param goal_state: The goal state to get to from the initial state.
+    :return: A tuple containing: a dictionary where each key is a state, and
+    each value is that state's parent, and the goal state. None is returned if
+    no path is found.
+    """
+    frontier: CakeHeap = CakeHeap()
+    visited: List = []
+    path: Dict[PancakeStack, PancakeStack] = {initial_state: None}
     frontier.push(initial_state, 0)
 
     while not frontier.empty():
-        current_state = frontier.pop()
+        current_state: PancakeStack = frontier.pop()
         visited.append(current_state)
         if goal_state == current_state:
-            return path
-        children = current_state.children()
-        for child in children:
+            return path, current_state
+        children: List[PancakeStack] = current_state.children()
+        for child in children:  # type: PancakeStack
             if child not in visited and child not in frontier:
                 frontier.push(child, child.cost_to_self())
                 path[child] = current_state
@@ -25,4 +34,4 @@ def uniform_cost(initial_state: PancakeStack, goal_state: PancakeStack) -> \
                 frontier.remove(child)
                 frontier.push(child, child.cost_to_self())
                 path[child] = current_state
-    return {}
+    return None
